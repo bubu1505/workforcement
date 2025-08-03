f1c37679b58378436be54ec3131c37e51e809a92
Gyana <gyanas2001@gmail.com>
2025-08-03 08:48:13 +0530

    first commit

---
+++ src/main/java/com/ralise/hiring/workforcement/WorkforcementApplication.java
@@ -0,0 +1,13 @@
+package com.ralise.hiring.workforcement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforcementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforcementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ralise/hiring/workforcement/WorkforcementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralise.hiring.workforcement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforcementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
